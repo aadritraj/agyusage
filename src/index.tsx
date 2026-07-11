3b2ce2d028ecacabfdfc375df4e74f1fc68565c0
aadritraj <176519217+aadritraj@users.noreply.github.com>
2026-07-11 17:44:09 +0200

    top level await
    since this project is heavily reliant on bun, supporting other runtimes that dont support top level await is kinda unnecessary

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,20 @@ import { render } from "ink";
 import { App } from "./ui/App";
 import { COMMANDS, printHelp } from "./cli/commands";
 
-const main = async (): Promise<void> => {
-  const args = Bun.argv.slice(2);
+const args = Bun.argv.slice(2);
 
-  if (args.length === 0) {
-    render(<App />);
-  } else {
-    const inputCmd = args[0].toLowerCase();
-    const command = COMMANDS.find((c) => c.name === inputCmd || c.aliases?.includes(inputCmd));
+if (args.length === 0) {
+  render(<App />);
+} else {
+  const command = COMMANDS.find(
+    (c) => c.name === args[0].toLowerCase() || c.aliases?.includes(args[0].toLowerCase()),
+  );
 
-    if (command) {
-      await command.action();
-    } else {
-      console.error(`Unknown subcommand: ${args[0]}`);
-      printHelp();
-      process.exit(1);
-    }
+  if (!command) {
+    console.error(`Unknown subcommand: ${args[0]}`);
+    printHelp();
+    process.exit(1);
   }
-};
 
-main();
+  await command.action();
+}
